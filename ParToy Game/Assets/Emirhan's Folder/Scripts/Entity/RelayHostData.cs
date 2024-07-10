d4d68e424ff42aeaa2599a416ad9d9a543606e5d
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2024-07-10 20:15:23 +0300

    Differnet bug

---
+++ ParToy Game/Assets/Emirhan's Folder/Scripts/Entity/RelayHostData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Emirhan_s_Folder.Scripts.Entity
+{
+    public struct RelayHostData
+    {
+        public string JoinCode;
+        public string IPv4Address;
+        public ushort Port;
+        public Guid AllocationID;
+        public byte[] AllocationIdBytes;
+        public byte[] ConnectionData;
+        public byte[] Key;
+
+    }
+}
---
+++ ParToy Game/Assets/Emirhan's Folder/Scripts/Entity/RelayJoinData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Emirhan_s_Folder.Scripts.Entity
+{
+    public struct RelayJoinData
+    {
+        public string IPv4Address;
+        public ushort Port;
+        public Guid AllocationID;
+        public byte[] AllocationIDBytes;
+        public byte[] ConnectionData;
+        public byte[] HostConnectionData;
+        public byte[] Key;
+    }
+}
